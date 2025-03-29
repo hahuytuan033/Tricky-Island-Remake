5ece4f2db6094e501c6bc4a61e6018c4045b921d
Huy Tuấn <163986199+hahuytuan033@users.noreply.github.com>
2025-03-29 14:26:05 +0700

    Jump and Flip

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -4,9 +4,17 @@ public class PlayerManager : MonoBehaviour
 {
     [Header("Player Settings")]
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+
+    [Header("Collision Settings")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask groundLayer;
+    private bool isGrounded;
+
     private Rigidbody2D rb;
     private float xInput;
     private Animator anim;
+    private bool facingRight = true;
 
     private void Awake()
     {
@@ -17,18 +25,53 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        xInput = Input.GetAxisRaw("Horizontal");
+        Collision();
+        PlayerInput();
+        Movement();
         Animations();
-        HandleMovement();
+        Flip();
+    }
+
+    private void PlayerInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    private void Collision()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundLayer);
+    }
+
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
     }
 
     private void Animations()
     {
         anim.SetFloat("xVelocity", rb.linearVelocity.x);
+        anim.SetFloat("yVelocity", rb.linearVelocity.y);
+        anim.SetBool("isGrounded", isGrounded);
     }
 
-    private void HandleMovement()
+    private void Movement()
     {
         rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    private void Flip()
+    {
+        if (xInput > 0 && !facingRight || xInput < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
     }
 }
