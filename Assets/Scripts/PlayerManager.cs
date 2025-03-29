1de39d3cf5e2ecf948e0790751453e49a6e0ce8d
Huy Tuấn <163986199+hahuytuan033@users.noreply.github.com>
2025-03-29 12:28:48 +0700

    movement and jump, fall

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -6,17 +6,25 @@ public class PlayerManager : MonoBehaviour
     [SerializeField] private float moveSpeed;
     private Rigidbody2D rb;
     private float xInput;
+    private Animator anim;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         xInput = Input.GetAxisRaw("Horizontal");
+        Animations();
         HandleMovement();
+    }
+
+    private void Animations()
+    {
+        anim.SetFloat("xVelocity", rb.linearVelocity.x);
     }
 
     private void HandleMovement()
